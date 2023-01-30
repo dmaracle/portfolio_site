92006563636bbb1d6cf3d33c847f12ef25d80a70
dmaracle <56905948+dmaracle@users.noreply.github.com>
2023-01-30 11:52:53 -0800

    add photo
    add photo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
-import tennis_img from '../../assets/images/tennis_img.jpg';
+import body_shot from '../../assets/images/body_shot.jpg';
 
 export const Home = () => {
   return (
@@ -18,7 +18,7 @@ export const Home = () => {
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
             className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(${tennis_img})` }}
+            style={{ backgroundImage: `url(${body_shot})` }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
